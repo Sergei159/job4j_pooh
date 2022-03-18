f5b5cb9890ab94be4393bacd1f2906a61115242b
sergei159 <89082766060QAzQAz>
2022-03-18 17:57:06 +0500

    second commit

--- src/main/java/ru/job4j/pooh/QueueService.java
+++ src/main/java/ru/job4j/pooh/QueueService.java
@@ -1,8 +1,32 @@
 package ru.job4j.pooh;
 
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class QueueService implements Service {
+    private final ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> queue
+            = new ConcurrentHashMap<>();
+
     @Override
     public Resp process(Req req) {
-        return null;
+        String text = req.getParam();
+        String sourceName = req.getSourceName();
+        String status = "200";
+
+        if ("POST".equals(req.httpRequestType())) {
+            queue.putIfAbsent(sourceName, new ConcurrentLinkedQueue<>());
+            queue.get(sourceName).add(text);
+            if ("".equals(text)) {
+                status = "204";
+            }
+            return new Resp(text, status);
+
+        } else if (("GET".equals(req.httpRequestType()))) {
+            String textResult = queue.get(sourceName).poll();
+            return new Resp(textResult, status);
+        } else {
+            throw new IllegalArgumentException("Wrong query");
+        }
     }
 }
